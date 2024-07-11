71cee5a367c197b437a0dacb83c965587db82786
helldevelopment <142198214+helldevelopment@users.noreply.github.com>
2024-07-10 18:20:41 -0700

    Update Anti-Client-Kick.lua

--- Support/Anti-Client-Kick.lua
+++ Support/Anti-Client-Kick.lua
@@ -3,13 +3,8 @@ namecall = hookmetamethod(game, "__namecall", function(self, ...)
     if self == game.Players.LocalPlayer and getnamecallmethod():lower() == "kick" then
         return nil
     end
-    return namecall(self, ...)
-end)
-
-local namecall2
-namecall2 = hookmetamethod(game, "__namecall", function(self, ...)
     if self == game.Players.LocalPlayer and getnamecallmethod():lower() == "destroy" then
         return nil
     end
-    return namecall2(self, ...)
+    return namecall(self, ...)
 end)
