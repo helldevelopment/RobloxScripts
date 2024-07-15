21347f431e0aeb930232b0e2e86617d67af4a58d
helldevelopment <142198214+helldevelopment@users.noreply.github.com>
2024-07-14 21:04:59 -0700

    Update MM2Loader.lua

--- Games/MM2/MM2Loader.lua
+++ Games/MM2/MM2Loader.lua
@@ -1,1 +1,40 @@
+local HookmetamethodSuccess = false
 
+function getGlobal(path)
+	local value = getfenv(0)
+
+	while value ~= nil and path ~= "" do
+		local name, nextValue = string.match(path, "^([^.]+)%.?(.*)$")
+		value = value[name]
+		path = nextValue
+	end
+
+	return value
+end
+
+function test(name,aliases, callback) -- taken and modified from the unc test
+	task.spawn(function()
+  		if getGlobal(name) then
+  			local success, message = pcall(callback)
+  	
+  			if success then
+            HookmetamethodSuccess = true
+  			end
+  		end
+
+	  end)
+end
+
+test("hookmetamethod", {}, function()
+	local object = setmetatable({}, { __index = newcclosure(function() return false end), __metatable = "Locked!" })
+	local ref = hookmetamethod(object, "__index", function() return true end)
+	assert(object.test == true, "Failed to hook a metamethod and change the return value")
+	assert(ref() == false, "Did not return the original function")
+end)
+
+if HookmetamethodSuccess then
+    print("Hookmetamethod test successful, loading sanity.wtf")
+else
+    print("Hookmetamethod test failed, loading sanity.lite")
+    
+end
