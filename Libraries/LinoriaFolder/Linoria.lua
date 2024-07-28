642a2a1097073fe6f65d041beb16f9ca755a1841
helldevelopment <142198214+helldevelopment@users.noreply.github.com>
2024-07-27 20:53:17 -0700

    Update Linoria.lua

--- Libraries/LinoriaFolder/Linoria.lua
+++ Libraries/LinoriaFolder/Linoria.lua
@@ -32,8 +32,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0,12,255)
-    OutlineColor = Color3.fromRGB(0,0,0)
+    AccentColor = Color3.fromRGB(0,12,255);
+    OutlineColor = Color3.fromRGB(0,0,0);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
