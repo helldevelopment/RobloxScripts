eda2e48b910c2d1d7da5e2d363981acd8803b988
helldevelopment <142198214+helldevelopment@users.noreply.github.com>
2024-08-20 18:15:18 -0700

    Update RivalsMain.lua

--- Games/Rivals/RivalsMain.lua
+++ Games/Rivals/RivalsMain.lua
@@ -1,6 +1,6 @@
 local Executor = identifyexecutor()
 
-if string.match(Executor, "Synapse") or string.match(Executor, "Wave") or string.match(Executor,"Rebel") then
+if string.match(Executor, "Synapse") or string.match(Executor, "Wave") or string.match(Executor,"Rebel") or string.match(Executor,"MacSploit") or string.match(Executor,"Macsploit") then
     print("Loading Rivals for PC")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/helldevelopment/RobloxScripts/main/Games/Rivals/RivalsPC.lua"))()
 else
