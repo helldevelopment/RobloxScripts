7808e734d3cfbc78fdd041008ebd90eeeaa52f0a
helldevelopment <142198214+helldevelopment@users.noreply.github.com>
2024-10-12 17:04:14 -0700

    Update RivalsMain.lua

--- Games/Rivals/RivalsMain.lua
+++ Games/Rivals/RivalsMain.lua
@@ -1,8 +1,15 @@
 local Executor = identifyexecutor()
 
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+local MessageBox = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/NotificationGUI/main/source.lua"))()
 
-Notification.new("error", "Error", "Rivals script is patched and detected, please wait until Rivals V2 releases",true,10) -- Args(<string> Type, <string> Heading, <string> Body, <boolean?> AutoRemoveNotif, <number?> AutoRemoveTime, <function?> OnCloseFunction)
+
+MessageBox.Show({Position = UDim2.new(0.5,0,0.5,0), Text = "sanity.wtf", Description = "Current Rivals script is patched and detected, \n please wait until Rivals V2 releases. \n Join discord.gg/sanitywtf for more information about V2", MessageBoxIcon = "Error", MessageBoxButtons = "OK", Result = function(res)
+   if (res == "Yes") then
+       MessageBox.Show({MessageBoxButtons = "OK", Description = "Wow, you said Yes! Thank you", Text = "YAYYY!"})
+   elseif (res == "No") then
+       MessageBox.Show({MessageBoxButtons = "OK", Description = "Ahh, sorry to dissapoint, I'll try better next time!", Text = "Nooooo"})
+   end
+end})
 --[[
 
 if string.match(Executor, "Synapse") or string.match(Executor, "Wave") or string.match(Executor,"Rebel") or string.match(Executor,"MacSploit") or string.match(Executor,"macsploit is") then
